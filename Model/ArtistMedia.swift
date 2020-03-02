2d123a802410f085ff005e48624d0081de58c983
Nathan Ngobale <nngobale@jhb.dvt.co.za>
2020-03-02 09:04:52 +0200

    Added Model

---
+++ Model/ArtistMedia.swift
@@ -0,0 +1,23 @@
+//
+//  ArtistMedia.swift
+//  NLibrary
+//
+//  Created by Nathan Ngobale on 2020/03/02.
+//
+
+import Foundation
+
+struct ArtistMediaResponse: Decodable {
+    var results: [Collection]
+}
+
+struct Collection: Decodable {
+    var artistName: String
+    var collectionName: String
+    var releaseDate: String
+    var artworkUrl30: String
+    var collectionPrice: Double
+    var trackTimeMillis: Int
+    var artworkUrl60: String
+    var previewUrl: String
+}

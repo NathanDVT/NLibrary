bb78b42017aac88cf79dee7f7a45b9022a5b7bd2
NathanDVT <nngobale@jhb.dvt.co.za>
2020-03-20 18:03:30 +0200

    Added project files

---
+++ UserMVVM/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserProtocol.swift
+//  taylorswift
+//
+//  Created by Nathan Ngobale on 2020/03/20.
+//  Copyright © 2020 Nathan Ngobale. All rights reserved.
+//
+import Foundation
+
+struct User {
+    var name: String
+    var surname: String
+    var id: String
+}

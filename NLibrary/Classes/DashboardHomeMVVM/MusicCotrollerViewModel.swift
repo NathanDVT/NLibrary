ce965d9820603307232557782cb8d8376323ddba
NathanDVT <nngobale@jhb.dvt.co.za>
2020-04-21 23:41:01 +0200

    Added music control bar to screens

---
+++ NLibrary/Classes/DashboardHomeMVVM/MusicCotrollerViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  UserProtocol.swift
+//  taylorswift
+//
+//  Created by Nathan Ngobale on 2020/03/20.
+//  Copyright © 2020 Nathan Ngobale. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+public class MusicControllerViewModel {
+    lazy var musicPlayer: AVPlayer = {return AVPlayer()}()
+    var name: String = "Select Artist!!!"
+
+    public init() {
+    }
+
+    public func playFromUrl(urlString: String, title: String) {
+            guard let url = URL.init(string: urlString) else {
+            return
+        }
+        let playerItem: AVPlayerItem = AVPlayerItem(url: url)
+        self.musicPlayer.replaceCurrentItem(with: playerItem)
+        musicPlayer.play()
+        self.name = title
+    }
+
+    public func getArtistName() -> String {
+        return name
+    }
+
+    public func isPlaying() -> Bool {
+        return musicPlayer.rate != 0;
+    }
+
+    public func pauseOrPlayCurrentSong() {
+        guard musicPlayer.error == nil else {
+            return
+        }
+        if musicPlayer.rate != 0 {
+            musicPlayer.pause()
+        } else {
+            musicPlayer.play()
+        }
+    }
+}

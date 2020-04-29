9e81524be39923001b1c8740c6513d7b42a05d98
NathanDVT <nngobale@jhb.dvt.co.za>
2020-04-29 23:55:26 +0200

    Added apple watch

--- NLibrary/Classes/ProfileMVVM/Prototypes/ProfileViewControllerProtocol.swift
+++ NLibrary/Classes/ProfileMVVM/Prototypes/ProfileViewControllerProtocol.swift
@@ -5,5 +5,4 @@ public protocol ProfileViewControllerProtocol: class {
     func successfulRequest(profileModel: ProfileModel)
     func successfulLogout()
     func failedProfileRequest(errorMessage: String)
-    func failedLogoutRequest(errorMessage: String)
 }
